2a37fad67ca5de8d8da75ec0ed748cbbed9ed070
marilise <marilise.morona@prestadores.intermedica.com.br>
2023-11-11 08:18:47 -0300

    Add FontStyle

---
+++ POC_CustomizeYourOrder/POC_CustomizeYourOrder/Commons/Extensions/UIFont+Nunito.swift
@@ -0,0 +1,26 @@
+//
+//  UIFont+Nunito.swift
+//  POC_CustomizeYourOrder
+//
+//  Created by marilise morona on 11/11/23.
+//
+
+import UIKit
+
+extension UIFont {
+    static var nunitoBold12: UIFont {
+        return UIFont(name: "Nunito-Bold", size: 12)!
+    }
+    
+    static var nunitoRegular12: UIFont {
+        return UIFont(name: "Nunito-Regular", size: 12)!
+    }
+    
+    static var nunitoExtraBold12: UIFont {
+        return UIFont(name: "Nunito-ExtraBold", size: 12)!
+    }
+    
+    static var nunitoSemiBold12: UIFont {
+        return UIFont(name: "Nunito-SemiBold", size: 12)!
+    }
+}
